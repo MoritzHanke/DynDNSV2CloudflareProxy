d78f74a4186925ad72fbdf39a5db9de1ec14de24
Moritz <lemma.eins@gmail.com>
2025-04-26 09:29:18 +0200

    extracted and renamed config values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import com.sun.security.auth.login.ConfigFile;
 import config.Config;
 import server.DyndnsV2Proxy;
 
@@ -15,14 +14,22 @@ import java.util.logging.SimpleFormatter;
 
 public class Main {
 
+    static String CONFIG_DEFAULT_KEY_runHTTP = "HTTP_active";
+    static String CONFIG_DEFAULT_KEY_HTTPPort = "HTTP_Port";
+    static String CONFIG_DEFAULT_KEY_runHTTPS = "HTTPS_active";
+    static String CONFIG_DEFAULT_KEY_HTTPsPort = "HTTPS_Port";
+    static String CONFIG_DEFAULT_KEY_keyStoreFilePath = "HTTPS_keyStoreFilePath";
+    static String CONFIG_DEFAULT_KEY_keyStorePassPhrase = "HTTPS_keyStorePassPhrase";
+    static String CONFIG_DEFAULT_KEY_IPAddress = "IPAddress";
+
     static String[] configDefaults = {
-        "runHTTP", "false",
-        "HTTPPort", "80",
-        "runHTTPS", "false",
-        "HTTPsPort", "443",
-        "keyStoreFilePath", "",
-        "keyStorePassPhrase", "",
-        "IPAddress", "127.0.0.1", //HTTPS not supported
+        CONFIG_DEFAULT_KEY_runHTTP, "false",
+        CONFIG_DEFAULT_KEY_HTTPPort, "80",
+        CONFIG_DEFAULT_KEY_runHTTPS, "false",
+        CONFIG_DEFAULT_KEY_HTTPsPort, "443",
+        CONFIG_DEFAULT_KEY_keyStoreFilePath, "",
+        CONFIG_DEFAULT_KEY_keyStorePassPhrase, "",
+        CONFIG_DEFAULT_KEY_IPAddress, "127.0.0.1",
     };
 
     static String defConfigPath = "./proxy.conf";
@@ -66,31 +73,30 @@ public class Main {
         DyndnsV2Proxy httpServer = null;
         DyndnsV2Proxy httpsServer = null;
 
-        String ip = properties.getProperty("IPAddress");
+        String ip = properties.getProperty(CONFIG_DEFAULT_KEY_IPAddress);
 
         if(ip == null){
             logger.log(Level.SEVERE, "missing IPAddress Field");
             System.err.println("missing IPAddress Field");
         }
 
-        if(Boolean.parseBoolean(properties.getProperty("runHTTP"))){
+        if(Boolean.parseBoolean(properties.getProperty(CONFIG_DEFAULT_KEY_runHTTP))){
             try {
-                int port = Integer.parseInt(properties.getProperty("HTTPPort"));
+                int port = Integer.parseInt(properties.getProperty(CONFIG_DEFAULT_KEY_HTTPPort));
                 httpServer = new DyndnsV2Proxy(ip, port);
                 System.out.printf("HTTP-Proxy started. Listening on %s:%d\n",ip, port);
             }catch (Exception e){
-                e.printStackTrace();
                 logger.log(Level.SEVERE, e.getMessage(), e);
             }
         }
 
         //HTTPS not supported (cant test this shit... )
 
-        if(Boolean.parseBoolean(properties.getProperty("runHTTPS"))){
+        if(Boolean.parseBoolean(properties.getProperty(CONFIG_DEFAULT_KEY_runHTTPS))){
             try {
-                int port = Integer.parseInt(properties.getProperty("HTTPsPort"));
-                String keyStoreFilePath = properties.getProperty("keyStoreFilePath");
-                String keyStorePassPhrase = properties.getProperty("keyStorePassPhrase");
+                int port = Integer.parseInt(properties.getProperty(CONFIG_DEFAULT_KEY_HTTPsPort));
+                String keyStoreFilePath = properties.getProperty(CONFIG_DEFAULT_KEY_keyStoreFilePath);
+                String keyStorePassPhrase = properties.getProperty(CONFIG_DEFAULT_KEY_keyStorePassPhrase);
                 httpsServer = new DyndnsV2Proxy(ip, port, keyStoreFilePath, keyStorePassPhrase);
                 System.out.printf("HTTPs-Proxy started. Listening on %s:%d\n",ip, port);
             }catch (Exception e){
